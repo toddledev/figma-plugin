5672a00bdcf911948e31b6edd59675cb4c0cd3ec
Mims H. Wright <mims@mimswright.com>
2025-02-25 13:33:26 +0100

    removed backend code for unused frameworks

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,4 @@
-// export { flutterMain } from "./flutter/flutterMain";
 export { htmlMain } from "./html/htmlMain";
-export { tailwindMain } from "./tailwind/tailwindMain";
-// export { swiftuiMain } from "./swiftui/swiftuiMain";
 export { run } from "./code";
 export { convertNodesToAltNodes as convertIntoNodes } from "./altNodes/altConversion";
 export * from "./messaging";
